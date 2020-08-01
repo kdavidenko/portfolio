cbb2f67677802d70bc56f1adb685894bcae1d769
Kyle D <kdavidenko@gmail.com>
2020-08-01 15:26:20 -0400

    Updates

---
+++ precache-manifest.6ffba66bddc8458168f90b3e05e1915b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1102f9cf2450cf6eae27fc493d17841",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ee79124fec043890829d",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee79124fec043890829d",
+    "url": "/portfolio/static/js/main.b9465356.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
