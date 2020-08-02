7cf9f1f6dd853d75af50cdbd30b5e84448d27c3e
Kyle D <kdavidenko@gmail.com>
2020-08-01 22:56:06 -0400

    Add design components and visual art

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+import CactusSVG from './components/cactus/cactus';
+import SkullSVG from './components/skull/skull';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Kyle's React Page
-        </p>
+    <>
+    <div className="site-background">
+      <header>
+        <div className="center title-1 bold block-12 brand-heavy">
+          Kyle Davidenko
+        </div>
+        <div className="center title-2 block-12 brand-heavy">
+          Front End Engineer
+        </div>
       </header>
+      <div className="cactus cactus-one"><CactusSVG></CactusSVG></div>
+      <div className="cactus cactus-two"><CactusSVG></CactusSVG></div>
+      <div className="skull"><SkullSVG></SkullSVG></div>
     </div>
+    </>
   );
 }
 
